490171c7baa48140b22c9f6c8668e5dd1152dd12
jessica andreu <jessikitaandreu@gmail.com>
2025-11-19 19:28:09 +0000

    Build Error
    Export Wheelchair doesn't exist in target module

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,8 +1,8 @@
 import type { LucideProps } from 'lucide-react';
-import { Wheelchair, Users, User, Mountain, PawPrint, Bus } from 'lucide-react';
+import { Accessibility, Users, User, Mountain, PawPrint, Bus } from 'lucide-react';
 
 export const Icons = {
-  Accessible: (props: LucideProps) => <Wheelchair {...props} />,
+  Accessible: (props: LucideProps) => <Accessibility {...props} />,
   Family: (props: LucideProps) => <Users {...props} />,
   Singles: (props: LucideProps) => <User {...props} />,
   Nature: (props: LucideProps) => <Mountain {...props} />,
