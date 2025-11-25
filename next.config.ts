d8bfaaa08066f18c4e146f3c4cf050fa01b56ae0
jessica andreu <jessikitaandreu@gmail.com>
2025-11-25 15:33:49 +0000

    https://concepto.de/wp-content/uploads/2020/10/animales-de-granja-e16041

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'concepto.de',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
